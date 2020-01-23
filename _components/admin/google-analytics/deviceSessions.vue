1353e4b04b072e9d28806808ec875b6d6a042ed1
Marcos Rativa <marcosr@imaginacolombia.com>
2020-01-23 16:20:23 -0500

    +Added: mostVisitedPages component +Fix: BrowserSessions and deviceSessions components

--- _components/admin/google-analytics/deviceSessions.vue
+++ _components/admin/google-analytics/deviceSessions.vue
@@ -89,36 +89,6 @@
                      }
                   ]
                   this.chartOptions.series = series
-                  /*  [
-                   {
-                      name: 'Brands',
-                      colorByPoint: true,
-                      data: [{
-                         name: 'Chrome',
-                         y: 61.41,
-                         sliced: true,
-                         selected: true
-                      }, {
-                         name: 'Internet Explorer',
-                         y: 11.84
-                      }, {
-                         name: 'Firefox',
-                         y: 10.85
-                      }, {
-                         name: 'Edge',
-                         y: 4.67
-                      }, {
-                         name: 'Safari',
-                         y: 4.18
-                      }, {
-                         name: 'Other',
-                         y: 7.05
-                      }]
-                   }
-                ]*/
-
-
-                  console.log(this.chartOptions.series, series)
                   resolve(true)//Resolve
                }).catch(error => {
                   this.$alert.error({message: this.$tr('ui.message.errorRequest'), pos: 'bottom'})
